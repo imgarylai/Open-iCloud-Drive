92a3b05b9e5598b7298579c0b21e0aad827246c9
Gary Lai <garylai1990@gmail.com>
2019-10-13 10:51:16 +0800

    Initial Commit

---
+++ Open iCloud Drive/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Open iCloud Drive
+//
+//  Created by Gary Lai on 10/13/19.
+//  Copyright © 2019 Gary Lai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
